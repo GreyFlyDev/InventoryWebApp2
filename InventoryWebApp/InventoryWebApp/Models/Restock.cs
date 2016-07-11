98a718435643527289027ea2cda7e7f9ad85a67e
GreyFlyDev <gregnnylf94@gmail.com>
2016-07-11 14:15:02 -0400

    Adde Restock Model

---
+++ InventoryWebApp/InventoryWebApp/Models/Restock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryWebApp.Models
+{
+    public class Restock
+    {
+        public int RestockId { get; set; }
+        public int QuantityPurchased { get; set; }
+        public decimal PurchasePricePerUnit { get; set; }
+
+        public string UserId { get; set; }
+        public int ProductId { get; set; }
+    }
+}
