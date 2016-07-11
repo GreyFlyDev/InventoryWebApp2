275624f2d874ecc27b90d0332f8842e0ef8241c3
GreyFlyDev <gregnnylf94@gmail.com>
2016-07-11 13:35:02 -0400

    Initial + PRoduct Model

---
+++ InventoryWebApp/InventoryWebApp/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryWebApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal PurchasePricePerUnit { get; set; }
+        public decimal SalePricePerUnit { get; set; }
+        public int NumberOfSales { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
